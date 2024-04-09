e2bbc8c943f37b40a07ac3d10a867fd68cba6d23
Abi-097 <120549516+Abi-097@users.noreply.github.com>
2024-04-10 05:17:31 +0530

    time formatted changed

--- app/components/TicketCard.js
+++ app/components/TicketCard.js
@@ -4,6 +4,20 @@ import ProgressBar from "./ProgressBar";
 import Status from "./Status";
 
 const TicketCard = ({ ticket }) => {
+  const formatTimestamp = (timestamp) => {
+    const options = {
+      year: "numeric",
+      day: "2-digit",
+      month: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    };
+
+    const date = new Date(timestamp);
+    const formattedDate = date.toLocaleString("en-IN", options);
+    return formattedDate;
+  };
   return (
     <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
       <div className="flex mb-3 ">
@@ -18,7 +32,7 @@ const TicketCard = ({ ticket }) => {
       <div className="flex-grow">
         <div className="flex mt-2">
           <div className="flex flex-col">
-            <p className="text-xs my-1 ">{ticket.updatedAt}</p>
+            <p className="text-xs my-1 ">{formatTimestamp(ticket.updatedAt)}</p>
             <ProgressBar progress={ticket.progress} />
           </div>
           <div className="ml-auto flex items-end">
